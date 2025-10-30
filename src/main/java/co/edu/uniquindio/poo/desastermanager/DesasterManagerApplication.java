1eca7f93c4eaa924f8d3a6034215c68d48483aad
valentinaG <168238534+valenkarol@users.noreply.github.com>
2025-10-30 16:14:17 -0500

    Creacion con Spring Boot
    feat: creación proyecto con Spring Boot

---
+++ src/main/java/co/edu/uniquindio/poo/desastermanager/DesasterManagerApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uniquindio.poo.desastermanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesasterManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesasterManagerApplication.class, args);
+    }
+
+}
