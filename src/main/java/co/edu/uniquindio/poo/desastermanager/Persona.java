750f59f4db7053c791b3470b9b5e0e60284549cd
valentinaG <168238534+valenkarol@users.noreply.github.com>
2025-11-03 17:57:22 -0500

    Creacion clase abstracta persona
    feat: Creacion clase abstracta persona

--- src/main/java/co/edu/uniquindio/poo/desastermanager/Persona.java
+++ src/main/java/co/edu/uniquindio/poo/desastermanager/Persona.java
@@ -1,5 +1,46 @@
 package co.edu.uniquindio.poo.desastermanager;
 
-public class Persona {
+public abstract class Persona {
+    private String nombrePersona;
+    private String apellidosPersona;
+    private int edad;
+    private String idPersona;
+
+    // Constructor
+    public Persona(String nombrePersona, String apellidosPersona, int edad, String idPersona) {
+        this.nombrePersona = nombrePersona;
+        this.apellidosPersona = apellidosPersona;
+        this.edad = edad;
+        this.idPersona = idPersona;
+    }
+
+    // Getters
+    public String getNombrePersona() {
+        return nombrePersona;
+    }
+
+    public String getApellidosPersona() {
+        return apellidosPersona;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public String getIdPersona() {
+        return idPersona;
+    }
+
+    // Método abstracto
+    public abstract void crear();
+    public abstract Persona leer();
+    public abstract void actualizar();
+    public abstract void eliminar();
+
+
+    @Override
+    public String toString() {
+        return nombrePersona + " " + apellidosPersona + " (ID: " + idPersona + ", edad: " + edad + ")";
+    }
 }
 
