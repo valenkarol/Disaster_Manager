a67194df9c3a4d00619bd3c71a724f6be4a63130
valentinaG <168238534+valenkarol@users.noreply.github.com>
2025-11-16 17:38:45 -0500

    fix: main
    fix: main

--- src/main/java/co/edu/uniquindio/poo/desastermanager/DesasterManagerApplication.java
+++ src/main/java/co/edu/uniquindio/poo/desastermanager/DesasterManagerApplication.java
@@ -2,12 +2,29 @@ package co.edu.uniquindio.poo.desastermanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class DesasterManagerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DesasterManagerApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/")
+                        .allowedOrigins("http://localhost:5173")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
     }
 
 }
