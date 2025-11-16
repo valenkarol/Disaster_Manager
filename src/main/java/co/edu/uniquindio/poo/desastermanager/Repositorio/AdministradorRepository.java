e3461fa68792b12a5a7e0651caaf8c72a2a21cff
KarolMD <karolm.davilas@uqvirtual.edu.co>
2025-11-16 12:07:20 -0500

    feat: agregar findByEmail para login
    feat: agregar en las clases administradorRepository y OperadorEmergenciaRepository el findByEmail para el login

--- src/main/java/co/edu/uniquindio/poo/desastermanager/Repositorio/AdministradorRepository.java
+++ src/main/java/co/edu/uniquindio/poo/desastermanager/Repositorio/AdministradorRepository.java
@@ -4,6 +4,9 @@ import co.edu.uniquindio.poo.desastermanager.Modelo.Administrador;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AdministradorRepository extends MongoRepository<Administrador, String> {
+    Optional<Administrador> findByEmail(String email);
 }
--- src/main/java/co/edu/uniquindio/poo/desastermanager/Repositorio/OperadorEmergenciaRepository.java
+++ src/main/java/co/edu/uniquindio/poo/desastermanager/Repositorio/OperadorEmergenciaRepository.java
@@ -4,6 +4,9 @@ import co.edu.uniquindio.poo.desastermanager.Modelo.OperadorEmergencia;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface OperadorEmergenciaRepository extends MongoRepository<OperadorEmergencia, String> {
+    Optional<OperadorEmergencia> findByEmail(String email);
 }
