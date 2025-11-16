d08dce16634882f3686fd642098ac50c28e499d7
KarolMD <karolm.davilas@uqvirtual.edu.co>
2025-11-16 18:11:31 -0500

    fix: prueba 2.2
    fix: prueba 2.2

--- src/main/java/co/edu/uniquindio/poo/desastermanager/Controlador/AuthController.java
+++ src/main/java/co/edu/uniquindio/poo/desastermanager/Controlador/AuthController.java
@@ -4,13 +4,11 @@ import co.edu.uniquindio.poo.desastermanager.Modelo.Persona;
 import co.edu.uniquindio.poo.desastermanager.Servicios.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
